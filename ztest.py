fee749c1bbe3883443341aaee9f2ed77cccea66c
Dana Swanstrom <danaswanstrom@gmail.com>
2016-10-08 05:44:18 -0500

    Working word splitter in ztest.py

--- ztest.py
+++ ztest.py
@@ -1,11 +1,15 @@
 import sys
 import re
 
-vowels = 'aeiou'
-line = sys.stdin.readline()
+vowel_pattern = re.compile('[aeiou]')
 pattern = re.compile('[a-zA-Z0-9]+')
 p = re.compile('\d+')
+
+line = sys.stdin.readline()
+print (re.split('[\\s\\n]+', line))
 while line:
+	
     for word in pattern.findall(line):
-        print (word + '\t' + str(len(word)) + '\t' + '1')
+        print (word + '\t' + str(len(vowel_pattern.findall(word))) + '\t' + '1')
+       
     line = sys.stdin.readline()
