b940e7bd4b0f0edbe75c61ec49403ff18a5225d6
Dana Swanstrom <danaswanstrom@gmail.com>
2016-10-10 05:38:56 -0500

    Set up the very basic framework for mapper3.py

---
+++ mapper3.py
@@ -0,0 +1,12 @@
+import sys
+import re
+
+def main(args):
+    line = sys.stdin.readline()
+
+    while line:
+        
+        line = sys.stdin.readline()
+
+if __name__ == '__main__':
+    main(sys.argv)
