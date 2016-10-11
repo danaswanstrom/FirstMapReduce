67cca789352c6cb2eb6ea3557e81205e4e72c5ef
Dana Swanstrom <danaswanstrom@gmail.com>
2016-10-11 13:55:02 -0500

    First draft of the reducer

---
+++ Reducer3.py
@@ -0,0 +1,17 @@
+import sys
+
+current_combination = None
+current_recommendation = '0123456789'
+
+for line in sys.stdin:
+    line = line.strip()
+    combination, recommendation = line.split('\t', 1)
+    if current_combination == combination:
+        current_recommendation = ''.join(sorted(set(recommendation) & set(current_recommendation)))
+    else:
+        if current_combination:
+            print(current_combination + '\t' + current_recommendation)
+            current_combination = combination
+            current_recommendation = recommendation
+    if current_combination == combination:
+        print(current_combination + '\t' + current_recommendation)
