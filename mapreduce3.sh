8c09afc29d213341eff90485a3841f467562d6be
Dana Swanstrom <danaswanstrom@gmail.com>
2016-10-13 04:46:46 -0500

    Added python 3 to script.

--- mapreduce3.sh
+++ mapreduce3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python mapper3.py < input3.txt > intermediate3.txt
+python3 mapper3.py < input3.txt > intermediate3.txt
 sort intermediate3.txt > intersorted3.txt
-python reducer3.py < intersorted3.txt > output3.txt
+python3 reducer3.py < intersorted3.txt > output3.txt
 nano output3.txt
